3eaa88757202ac405eb2494dc6d6fcc8a9c8286e
Jonny Abrams <jonnyabrams@hotmail.com>
2022-07-25 17:00:34 +0100

    chore: prevent server-side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <h1 className='text-3xl font-bold underline'>
       TokTik
-    </div>
+    </h1>
   )
 }
 
