f6f6be4a26e350b0628b939b704b15de3e4984ee
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-01 08:46:40 +0100

    feat: can sign in with google OAuth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
+import { GoogleLogin, googleLogout } from "@react-oauth/google"
 import { useRouter } from "next/router"
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { AiOutlineLogout } from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 import Logo from '../utils/toktik-logo.png'
+import { createOrGetUser } from "../utils"
 
 const Navbar = () => {
+  const user = false
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href='/'>
@@ -20,6 +23,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>SEARCH</div>
+
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
   )
 }
