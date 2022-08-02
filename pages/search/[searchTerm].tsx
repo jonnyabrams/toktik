6b37c704d51d5f7e8de2b6c80747d5a7a42d87a6
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-02 18:25:47 +0100

    feat: complete search functionality

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -13,9 +13,13 @@ import useAuthStore from '../../store/authStore'
 
 const Search = ({ videos }: { videos: Video[] }) => {
   const [isAccounts, setIsAccounts] = useState(false)
+  const router = useRouter()
+  const { searchTerm }: any = router.query
+  const { allUsers } = useAuthStore()
 
   const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
   const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
+  const searchedAccounts = allUsers.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <div className='w-full'>
@@ -23,10 +27,52 @@ const Search = ({ videos }: { videos: Video[] }) => {
         <p onClick={() => setIsAccounts(true)} className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`}>
           Accounts
         </p>
-        <p onClick={() => setIsAccounts(false)} className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`}>
+        <p onClick={() => setIsAccounts(false)} className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`}>
           Videos
         </p>
       </div>
+      {isAccounts ? (
+        <div className='md:mt-16'>
+          {searchedAccounts.length > 0 ? (
+            searchedAccounts.map((user:IUser, index: number) => (  
+              <Link key={index} href={`/profile/${user._id}`}> 
+                <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+                  <div>
+                    <Image 
+                      src={user.image}
+                      width={50}
+                      height={50}
+                      className='rounded-full'
+                      alt='user-profile'
+                    />
+                  </div>
+                  <div className="hidden xl:block">
+                    <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                      {user.userName.replaceAll(' ', '')}
+                      <GoVerified className="text-blue-400" />
+                    </p>
+                    <p className="capitalize text-gray-400 text-xs">
+                      {user.userName}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <NoResults text={`No account results for ${searchTerm}`} />
+          )}
+        </div>
+      ) : (
+        <div className='md:mt-16 flex flex-wrap gap-6 md:justify-start'>
+          {videos.length ? (
+            videos.map((video: Video, index) => (
+              <VideoCard post={video} key={index} />
+            ))
+          ) : (
+            <NoResults text={`No video results for ${searchTerm}`} />
+          )}
+        </div>
+      )}
     </div>
   )
 }
