288117c103aa2d7dfac207835e72b5d98ed5fe65
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-02 15:41:38 +0100

    feat: Profile displays user name & image

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -17,8 +17,32 @@ interface IProps {
 }
 
 const Profile = ({ data }: IProps) => {
+  const { user, userVideos, userLikedVideos } = data
+
   return (
-    <div>Profile</div>
+    <div className='w-full'>
+      <div className='flex gap-6 md:gap-10 mb-4 bg-white w-full'> 
+        <div className="w-16 h-16 md:w-32 md:h-32">
+          <Image 
+            src={user.image}
+            width={120}
+            height={120}
+            className='rounded-full'
+            alt='user-profile'
+            layout='responsive'
+          />
+        </div>
+        <div className='flex flex-col justify-center'>
+          <p className="md:text-2xl tracking-wider flex gap-1 items-center justify-center text-md font-bold text-primary lowercase">
+            {user.userName.replaceAll(' ', '')}
+            <GoVerified className="text-blue-400" />
+          </p>
+          <p className="capitalize md:text-xl text-gray-400 text-xs">
+            {user.userName}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
@@ -27,7 +51,7 @@ export const getServerSideProps = async ({
 }: {
   params: { id: string }
 } ) => {
-  const res = await axios.get(`${BASE_URL}/profile/${id}`)
+  const res = await axios.get(`${BASE_URL}/api/profile/${id}`)
 
   return {
     props: { data: res.data }
