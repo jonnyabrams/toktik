c078508c53eba1d8da36fc994115dc21c34d8476
Jonny Abrams <jonnyabrams@hotmail.com>
2022-08-02 17:44:04 +0100

    feat: search page toggle between Accounts & Videos

---
+++ pages/api/search/[searchTerm].ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { searchPostsQuery } from '../../../utils/queries'
+import { client } from '../../../utils/client'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    const { searchTerm } = req.query
+
+    const videosQuery = searchPostsQuery(searchTerm)
+
+    const videos = await client.fetch(videosQuery)
+
+    res.status(200).json(videos)
+  }
+}
+
+export default handler
